5d2ee1f17efdaa81441bca300031238ba434d5ff
mmeier <michael.meier@huskers.unl.edu>
2020-01-02 12:46:52 -0600

    added password reset

--- repo/urls.py
+++ repo/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
-from . import views
-from .views import LickListView, LickDetailView, LickCreateView
+#from . import views
+from .views import (
+    about,
+    LickListView,
+    UserLickListView,
+    LickDetailView,
+    LickCreateView,
+    LickUpdateView,
+    LickDeleteView
+)
 
 urlpatterns = [
-    path('', views.LickListView.as_view(), name='home'),
-    path('about/', views.about, name='about'),
-    path('lick/<int:pk>/', views.LickDetailView.as_view(), name='lick-detail'),
-    path('lick/new/', views.LickCreateView.as_view(), name='lick-create'),
-    #path('lick/new/', views.lick_new, name='lick_new'),
-    #path('', views.LickListView.as_view(), name='licks'),
-    #path('lick/<uuid:pk>', views.LickDetailView.as_view(), name='lick-detail'),
+    path('', LickListView.as_view(), name='home'),
+    path('user/<str:username>/', UserLickListView.as_view(), name='user-licks'),
+    path('about/', about, name='about'),
+    path('lick/<int:pk>/', LickDetailView.as_view(), name='lick-detail'),
+    path('lick/new/', LickCreateView.as_view(), name='lick-create'),
+    path('lick/<int:pk>/update/', LickUpdateView.as_view(), name='lick-update'),
+    path('lick/<int:pk>/delete/', LickDeleteView.as_view(), name='lick-delete'),
 ]
--- repo/views.py
+++ repo/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 from repo.models import Lick
 from django.views import generic
 from .forms import LickForm
@@ -15,7 +19,18 @@ class LickListView(generic.ListView):
     template_name = 'repo/home.html'
     context_object_name = 'licks'
     ordering = ['-date_posted']  # minus reverses order
-    #paginate_by = 5
+    paginate_by = 5
+
+
+class UserLickListView(generic.ListView):
+    model = Lick
+    template_name = 'repo/user_licks.html'
+    context_object_name = 'licks'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Lick.objects.filter(author=user).order_by('-date_posted')
 
 
 class LickDetailView(generic.DetailView):
@@ -23,20 +38,62 @@ class LickDetailView(generic.DetailView):
     template_name = 'repo/lick_detail.html'
 
 
-class LickCreateView(generic.CreateView):
+class LickCreateView(SuccessMessageMixin, LoginRequiredMixin, generic.CreateView):
     model = Lick
     form_class = LickForm
     success_url = reverse_lazy('lick-create')
+    success_message = f'Lick has been created successfully, you may upload another one!'
     template_name = 'repo/lick_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+class LickUpdateView(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Lick
+    form_class = LickForm
+    success_url = reverse_lazy('home')
+    success_message = f'Lick has been successfully updated!'
+    template_name = 'repo/lick_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class LickDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Lick
+    template_name = 'repo/lick_confirm_delete.html'
+    success_url = reverse_lazy('home')
+    success_message = f'Lick has been deleted!'
+
+    def delete(self, request, *args, **kwargs):  # this replaces SuccessMessageMixin
+        messages.success(self.request, self.success_message)
+        return super(LickDeleteView, self).delete(request, *args, **kwargs)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+# redirecting back to home
+# redirect to home and make success message appear
+
+
 # check corey 26:30
 # https://www.youtube.com/watch?v=-s7e_Fy6NRU&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&index=10
 # https://www.youtube.com/watch?v=HSn-e2snNc8
 # trying to replace function based form with class based form to upload list
+
 
 
 # function based view that goes with lick_new.html template
