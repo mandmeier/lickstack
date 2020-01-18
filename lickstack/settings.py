bd17b4539bd90a50f2fc40826b27b0a4ddb5b82d
mmeier <michael.meier@huskers.unl.edu>
2020-01-18 17:57:48 -0600

    start working on responsive design

--- lickstack/settings.py
+++ lickstack/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    # if your static files folder is named "staticfiles"
+    os.path.join(BASE_DIR, 'lickstack/staticfiles'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
