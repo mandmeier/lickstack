673889616e9f969f0a894cbf5349c33cd4f98d54
mmeier <michael.meier@huskers.unl.edu>
2020-01-23 12:09:27 -0600

    added file extension and size validation to lick upload form

--- repo/forms.py
+++ repo/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Lick
+from django.core.exceptions import ValidationError
+import os
 
 
 class LickForm(forms.ModelForm):
@@ -17,44 +19,15 @@ class LickForm(forms.ModelForm):
     super().__init__(*args, **kwargs)
     self.fields['file'].widget.attrs['id'] = 'inpFile'
 
-
-"""
-
-class LickForm(forms.ModelForm):
-
-    class Meta:
-        model = Lick
-        fields = ('file', 'key1_1', 'key1_2', 'key1_3', 'key1_4', 'genre',)
-
-
-class LickForm(forms.ModelForm):
-
-    class Meta:
-        model = Lick
-        fields = ('file', 'instrument', 'key1_1',
-                  'key1_2', 'key1_3', 'key1_4', 'genre',)
-        widgets = {
-            'genre': forms.TextInput(attrs={'class': 'form-control'})
-        }
-
-
-
-    # Add some custom validation to check audio files
-     def clean_audio_file(self):
-         file = self.cleaned_data.get('file',False):
-         if file:
-             if file.size > 1*1024*1024:
-                   raise ValidationError("Audio file too large ( > 1mb )")
-             if not file.content-type in ["audio/mpeg","audio/wav"]:
-                   raise ValidationError("please submit mpeg")
-             if not os.path.splitext(file.name)[1] in [".mp3",".wav" ...]:
-                   raise ValidationError("Doesn't have proper extension")
-             # Here we need to now to read the file and see if it's actually
-             # a valid audio file. I don't know what the best library is to
-             # to do this
-             if not some_lib.is_audio(file.content):
-                   raise ValidationError("Not a valid audio file")
-             return file
-         else:
-             raise ValidationError("Couldn't read uploaded file")
-"""
+  # validate uploaded file for extension and file size
+  def clean_file(self):
+    file = self.cleaned_data['file']
+    valid_extensions = ['.m4a', '.mp3']
+    ext = os.path.splitext(file.name)[1]
+    if not ext.lower() in valid_extensions:
+      raise ValidationError('Please upload a .mp3 or .m4a audio file.')
+    elif file.size > 1 * 1024 * 1024:
+      raise forms.ValidationError(
+          f'File is too large ({str(round(file.size/(1024 * 1024), 2))} Mb). Please stay below 1 Mb. Consider shortening it.')
+    else:
+      return file
--- repo/models.py
+++ repo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+#from .validators import validate_audio
 
 
 class Genre(models.Model):
