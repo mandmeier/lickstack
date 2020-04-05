a295b25b88a554b8e4d07eb1bd459425991b37fb
mmeier <michael.meier@huskers.unl.edu>
2020-04-05 11:12:01 -0500

    secret key in settings

--- lickstack/settings.py
+++ lickstack/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7-drb0cmsro#idbs*#%v^1z+2yt-o771+r7+v@9tl5c##uoh!m'
+# SECRET_KEY = '7-drb0cmsro#idbs*#%v^1z+2yt-o771+r7+v@9tl5c##uoh!m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
