cf9ef47289ed1b730c13adea7c44a867631c227c
mmeier <michael.meier@huskers.unl.edu>
2020-04-07 12:11:09 -0500

    set debug value

--- lickstack/settings.py
+++ lickstack/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = [
     'localhost',
