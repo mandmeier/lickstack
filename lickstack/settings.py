e0a23e7d0807eb14e906d592e11eb93c84cf758f
mmeier <michael.meier@huskers.unl.edu>
2020-04-12 14:43:20 -0500

    added AWS S3 bucket

--- lickstack/settings.py
+++ lickstack/settings.py
@@ -32,7 +32,9 @@ DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 ALLOWED_HOSTS = [
     'localhost',
     'lickstack.herokuapp.com',
-    'lickstack.com', ]
+    'lickstack.com',
+    'localhost:8000',
+]
 
 
 # Application definition
@@ -48,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'storages',
+    'storages',
     'django_filters',
 ]
 
@@ -133,7 +135,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    #os.path.join(BASE_DIR, 'media'),
 ]
 
 STATIC_URL = '/static/'
@@ -157,14 +159,15 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
-# # AWS variables
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
+# AWS variables
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+#AWS_S3_HOST = 's3.ca-central-1.amazonaws.com'
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 TAGGIT_CASE_INSENSITIVE = True
