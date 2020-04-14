4ac5ea37fe78cd2c10b1559dba368f2d18b0bda3
mmeier <michael.meier@huskers.unl.edu>
2020-04-14 16:39:13 -0500

    staticfiles slim

--- lickstack/settings.py
+++ lickstack/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 
