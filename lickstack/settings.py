ecbf60fd014941dab3ace4608591990bbd2a1164
mmeier <michael.meier@huskers.unl.edu>
2020-04-22 11:22:21 -0500

    added sslforfree hidden folder, added info@lickstack.com email

--- lickstack/settings.py
+++ lickstack/settings.py
@@ -165,9 +165,11 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+#EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
