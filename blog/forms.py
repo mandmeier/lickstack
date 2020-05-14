436d64db1754c5d88a38c35ad3965f7c833d5862
mmeier <michael.meier@huskers.unl.edu>
2020-05-14 14:20:05 -0500

    blog form css

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,8 @@ class CreateArticle(forms.ModelForm):
     class Meta:
         model = models.Article
         fields = ['title', 'description', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs['id'] = 'textfield_description'
+        self.fields['body'].widget.attrs['id'] = 'textfield_body'
