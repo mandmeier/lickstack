34070f37b773749a47066964d95f2ac89b7265f2
mmeier <michael.meier@huskers.unl.edu>
2020-07-28 17:59:55 -0500

    admin page show user date created

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,17 @@
-from django.contrib import admin
+
 from .models import Profile
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    ordering = ('-date_joined', )
+    list_display = ('username', 'email', 'date_joined',
+                    'last_login', 'is_active')  # Added last_login
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Profile)
